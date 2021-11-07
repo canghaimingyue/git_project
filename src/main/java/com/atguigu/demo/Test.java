ed29f4500083ea315217db0822776cc45ecaeb4d
kuji <kuji@163.com>
2021-11-07 15:08:33 +0800

    branch01 第一次提交

--- src/main/java/com/atguigu/demo/Test.java
+++ src/main/java/com/atguigu/demo/Test.java
@@ -2,7 +2,7 @@ package com.atguigu.demo;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello  git1");
-        System.out.println("hello  git2");
+        System.out.println("hello  本科大一");
+        System.out.println("hello  本科大二");
     }
 }
