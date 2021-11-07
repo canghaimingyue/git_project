21c866961874c51aef617881d0152c3d983e695e
kuji <kuji@163.com>
2021-11-07 15:56:13 +0800

    branch01 第二次提交

--- src/main/java/com/atguigu/demo/Test.java
+++ src/main/java/com/atguigu/demo/Test.java
@@ -2,7 +2,10 @@ package com.atguigu.demo;
 
 public class Test {
     public static void main(String[] args) {
+        System.out.println("hello git1");
+        System.out.println("hello git2");
         System.out.println("hello  本科大一");
         System.out.println("hello  本科大二");
+        System.out.println("大学本科毕业");
     }
 }
